73a863873b70236067d1c2991efe3dcc85950a18
Javid <sairexm@gmail.com>
2024-06-10 16:19:14 +0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        //TASK 1
+        for(int i = 1; i < 11; i++)
+        {
+            System.out.println(i);
+        }
+
+        //TASK 2
+        for(int i = 10; i > 0; i--)
+        {
+            System.out.println(i);
+        }
+
+        //TASK 3
+        for(int i = 2; i <= 17; i = i + 2)
+        {
+            System.out.println(i);
+        }
+
+
+    }
+}
